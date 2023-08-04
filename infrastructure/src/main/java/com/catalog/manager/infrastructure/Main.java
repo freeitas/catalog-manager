5d4543cdd3121b3ef44e7cb2aad80f59fc146984
Guilherme Freitas <hfdsguilherme@gmail.com>
2023-08-04 00:11:15 -0300

    initial commit

---
+++ infrastructure/src/main/java/com/catalog/manager/infrastructure/Main.java
@@ -0,0 +1,15 @@
+package com.catalog.manager.infrastructure;
+
+import com.catalog.manager.application.UseCase;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.println(new UseCase().execute());
+
+    }
+}

150697fd31f1aa5917db7ed448dfcdb0e1f1c1b8
Guilherme Freitas <guilherme.f@onerhino.com>
2023-09-12 23:47:23 -0300

    chore: notification pattern

--- application/src/main/java/com/catalog/manager/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/com/catalog/manager/application/category/create/CreateCategoryUseCase.java
@@ -1,7 +1,9 @@
 package com.catalog.manager.application.category.create;
 
 import com.catalog.manager.application.UseCase;
+import com.catalog.manager.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
 public abstract class CreateCategoryUseCase
-        extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+        extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 }
